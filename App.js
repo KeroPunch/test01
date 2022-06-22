f650321d93808bb4027d44fc7f5e875a716e5c14
Kritsakorn Dechalert <de.kritsakorn_st@tni.ac.th>
2022-06-22 10:57:15 +0700

    new commit

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
-import { StyleSheet, View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import stySheet from './components/stySheet';
 import Logo from './components/Logo';
-import styles from './components/styles';
+
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={[styles.title,styles.warning]}>Hello React Native</Text>
-      <Logo/>
+      {/* <Text style={[styles.title,styles.warning]}>Hello React Native</Text> */}
+      {/* <Logo /> */}
+      <stySheet/>
     </View>
   )
 }
 
 export default App
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+  title: {
+    fontSize: 20,
+  },
+  warning: {
+    color: 'red'
+  }
+});
