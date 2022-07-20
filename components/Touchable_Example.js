3799ca1626dfc951a12b4332d9ef381bb8e0c92c
Kritsakorn Dechalert <de.kritsakorn_st@tni.ac.th>
2022-07-20 11:42:04 +0700

    7/20/2022

--- components/Touchable_Example.js
+++ components/Touchable_Example.js
@@ -1,69 +1,67 @@
 import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  SafeAreaView,
-} from "react-native";
-import React from "react";
-
-const Touchable_Example = () => {
-  const onPress = (msg) => {
-    alert("Alert for: " + msg);
+    StyleSheet,
+    Text,
+    View,
+    TouchableHighlight,
+    TouchableNativeFeedback,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    SafeAreaView
+  } from "react-native";
+  import React from "react";
+  
+  const Touchable_Example = () => {
+      const onPress = (msg) => {
+          alert('Alert for : '+msg)
+  
+      }
+    return (
+      <SafeAreaView style={{flex:1}}>
+          <View style={styles.container}>
+          <TouchableNativeFeedback 
+          onPress={()=>onPress('TouchableNativeFeedback')}
+          >
+              <View style={styles.button}>
+                  <Text>Touchable Native Feedback Only Android</Text>
+              </View>
+          </TouchableNativeFeedback>
+          <TouchableHighlight
+          style={styles.button}
+          onPress={()=> onPress('TouchableHighlight Pressed')}
+          >
+              <Text>Touchable Highlight</Text>
+          </TouchableHighlight>
+          <TouchableOpacity
+          style={styles.button}
+          onPress={()=> onPress('TouchableOpacity Pressed')}
+          >
+              <Text>Touchable Opacity</Text>
+          </TouchableOpacity>
+          <TouchableWithoutFeedback 
+          onPress={()=>onPress('TouchableNativeFeedback')}
+          >
+              <View style={styles.button}>
+                  <Text>Touchable Without Feedback</Text>
+              </View>
+          </TouchableWithoutFeedback>
+          </View>
+      </SafeAreaView>
+    );
   };
-
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <TouchableNativeFeedback
-          onPress={() => onPress("TouchableWithoutFeedback Pressed")}
-        >
-          <View style={styles.button}>
-            <Text>TouchableNWithoutFeedback(Only Android)</Text>
-          </View>
-        </TouchableNativeFeedback>
-        <TouchableHighlight
-          style={styles.button}
-          onPress={() => onPress("TouchableHighlight Pressed")}
-        >
-          <Text>TouchableHighlight(Only Android)</Text>
-        </TouchableHighlight>
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => onPress("TouchableOpacity Pressed")}
-        >
-          <Text>TouchableNOpacity(Only Android)</Text>
-        </TouchableOpacity>
-
-        <TouchableWithoutFeedback
-          onPress={() => onPress("TouchableWithoutFeedback Pressed")}
-        >
-          <View style={styles.button}>
-            <Text>TouchableNWithoutFeedback(Only Android)</Text>
-          </View>
-        </TouchableWithoutFeedback>
-      </View>
-    </SafeAreaView>
-  );
-};
-
-export default Touchable_Example;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    marginTop: 50,
-  },
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD", //color of button
-    padding: 10,
-    width: 300,
-    marginTop: 16,
-  },
-});
+  
+  export default Touchable_Example;
+  
+  const styles = StyleSheet.create({
+      container: {
+          flex: 1,
+          justifyContent: "center",
+          marginHorizontal: 16,
+        },
+      button:{
+          alignItems:"center",
+          backgroundColor:"#DDDDDD",
+          padding:10,
+          width:300,
+          marginTop:16
+      }
+  });
