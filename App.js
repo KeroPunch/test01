93213a4381c5cc3858b3777cb865042a6f99299f
Kritsakorn Dechalert <de.kritsakorn_st@tni.ac.th>
2022-07-27 09:39:35 +0700

    Commit 27/7/2022

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import {View, Text } from 'react-native'
 import React from 'react'
 /*import AlertComponent from './components/AlertComponent'*/
-/*import ImageWithTextInput from './components/ImageWithTextInput'*/
+import ImageWithTextInput from './components/ImageWithTextInput'
 /*import ButtonExample from './components/ButtonExample'*/
-import Touchable_Example from './components/Touchable_Example';
-
+/*import Touchable_Example from './components/Touchable_Example';*/
+import TouchablePractice from './components/TouchablePractice';
 const App = () => {
   return (
     <View style = {{flex:1}}>
-      <Touchable_Example/>
+      <TouchablePractice/>
     </View>
   )
 }
